6d51bead3c9df2032ed03138b2bc8b288663774b
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2025-02-27 18:40:41 +0530

    Add file reading functionality to app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("App.js");
+
+const fs = require("fs");
+fs.readFile("file.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
